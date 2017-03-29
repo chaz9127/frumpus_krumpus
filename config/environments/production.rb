966950ea6d72d39709315c81270f8306fc599c84
CJ <cj@gamersensei.com>
2017-03-28 20:26:36 -0400

    forgot prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: 'dev-frumpus-krumpus-comics',
+    bucket: 'prod-frumpus-krumpus-comics',
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     s3_region: ENV['AWS_REGION']
